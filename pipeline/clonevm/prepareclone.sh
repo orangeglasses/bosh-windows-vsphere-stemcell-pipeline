75f7c3b760549c8a65cac0171a98320863be64bf
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-17 14:08:25 +0200

    shell

--- pipeline/clonevm/prepareclone.sh
+++ pipeline/clonevm/prepareclone.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -x
+set -v
 
 echo $VROUSER
 VROAPI=$VROENDPOINT"/d9ad2397-ac07-444d-978e-5f86c07f09d5/executions"
 echo $VROAPI
 echo "Starting clone workflow at "$VROAPI" with user "$VROUSER
 
-RESPONSE=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' "'"$VROAPI"'" )
+RESPONSE=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' $VROAPI )
 echo "API Response: "$RESPONSE
 
 TOKEN=$($RESPONSE | grep Location: | cut -d' ' -f2)
