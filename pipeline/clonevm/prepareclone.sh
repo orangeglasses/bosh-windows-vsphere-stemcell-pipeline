d8e185eb95a73b906637b253a189c7b10f2d5d0c
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-17 15:55:42 +0200

    one more time

--- pipeline/clonevm/prepareclone.sh
+++ pipeline/clonevm/prepareclone.sh
@@ -3,10 +3,7 @@
 VROAPI=$VROENDPOINT"/d9ad2397-ac07-444d-978e-5f86c07f09d5/executions"
 echo "Starting clone workflow at "$VROAPI" with user "$VROUSER
 
-RESPONSE=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' $VROAPI )
-echo "API Response: "$RESPONSE
-
-TOKEN=$($RESPONSE | grep Location: | cut -d' ' -f2)
+TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' $VROAPI | grep Location: | cut -d' ' -f2 )
 echo "Status token: "$TOKEN
 
 # eliminate CR from result.
