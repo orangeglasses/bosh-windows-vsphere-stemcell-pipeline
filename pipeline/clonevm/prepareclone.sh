7d61845d88c438db3833ef077c6aa1dd02d85379
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-17 11:27:31 +0200

    more logging in clone

--- pipeline/clonevm/prepareclone.sh
+++ pipeline/clonevm/prepareclone.sh
@@ -4,11 +4,14 @@
 echo "Installing curl"
 apt-get update && apt-get install -y curl && apt-get clean && rm -rf /var/lib/apt/lists
 
-echo "Starting clone workflow"
 echo $VROUSER
-echo $VROENDPOINT
+VROAPI=$VROENDPOINT"/d9ad2397-ac07-444d-978e-5f86c07f09d5/executions"
+echo $VROAPI
+echo "Starting clone workflow at "$VROAPI" with user "$VROUSER
 
-TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' '$VROENDPOINT/d9ad2397-ac07-444d-978e-5f86c07f09d5/executions' | grep Location: | cut -d' ' -f2)
+TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' '$VROAPI' | grep Location: | cut -d' ' -f2)
+echo "Received: "$TOKEN
+
 # eliminate CR from result.
 TEST=""
 for data in $TOKEN ;
