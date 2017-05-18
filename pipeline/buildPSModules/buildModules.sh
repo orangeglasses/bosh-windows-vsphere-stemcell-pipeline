b610823000c8f7d80b7afb5be6cd19fcdf36c14b
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-18 11:07:00 +0200

    Building

--- pipeline/buildPSModules/buildModules.sh
+++ pipeline/buildPSModules/buildModules.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 echo $VERSION
+echo $PWD
+
+echo "Checking out version "$VERSION
+git checkout $VERSION
+
+echo "Getting submodules"
+git submodule update --init --recursive
+
+echo "Installing bundler"
+gem install bundler
+
+echo "Bundling"
+bundle install
+
+echo "Building modules"
+rake package:psmodules
 
 echo Modules ready.
 exit 0
