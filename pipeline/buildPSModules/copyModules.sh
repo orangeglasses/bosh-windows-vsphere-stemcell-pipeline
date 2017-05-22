b56a9049bb47ac2990a3f27316dd53584bad3d41
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-22 15:18:34 +0200

    Quotes

--- pipeline/buildPSModules/copyModules.sh
+++ pipeline/buildPSModules/copyModules.sh
@@ -6,6 +6,6 @@ echo "Current folder: "$PWD
 
 echo "Copying modules"
 # todo add ip returned from vro script
-sshpass -p $VMPASS scp BOSHmodules/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
+sshpass -p \"$VMPASS\" scp BOSHmodules/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
 
 echo Modules copied to VM.
