e7a854f3e252001d638ce10aba8b18da68c3d364
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-23 09:44:01 +0200

    unzip

--- pipeline/buildPSModules/copyModules.sh
+++ pipeline/buildPSModules/copyModules.sh
@@ -4,8 +4,12 @@ set -e
 
 echo "Current folder: "$PWD
 
+echo "Unzipping modules"
+mkdir BOSHmodulesExtracted
+unzip -o BOSHmodules/* -d BOSHmodulesExtracted/
+
 echo "Copying modules"
 # todo add ip returned from vro script
-sshpass -p $VMPASS scp -v -o StrictHostKeyChecking=no BOSHmodules/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
+sshpass -p $VMPASS scp -v -o StrictHostKeyChecking=no BOSHmodulesExtracted/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
 
 echo Modules copied to VM.
