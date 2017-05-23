52cda5f99b2d01b6f2ddf0454ff107662746204e
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-23 09:18:21 +0200

    disable strict host checking

--- pipeline/buildPSModules/copyModules.sh
+++ pipeline/buildPSModules/copyModules.sh
@@ -6,6 +6,6 @@ echo "Current folder: "$PWD
 
 echo "Copying modules"
 # todo add ip returned from vro script
-sshpass -p \"$VMPASS\" scp -v BOSHmodules/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
+sshpass -p \"$VMPASS\" scp -v -o StrictHostKeyChecking=no BOSHmodules/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
 
 echo Modules copied to VM.
