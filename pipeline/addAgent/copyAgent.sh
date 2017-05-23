df55c4a7fb02632a1b3f538a8c9d361974bd7995
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-23 12:28:47 +0200

    prepareCell

--- pipeline/addAgent/copyAgent.sh
+++ pipeline/addAgent/copyAgent.sh
@@ -8,4 +8,4 @@ echo "Copying agent."
 # todo add ip returned from vro script
 sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@192.168.0.80:'"/Program Files/WindowsPowerShell/Modules"'
 
-echo "Modules copied to VM."
+echo "Agent zip copied to VM."
---
+++ pipeline/prepareCell/prepareCell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Connecting to cell VM."
+# todo add ip returned from vro script
+sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@192.168.0.80
+
+echo "Installing CF Features."
+powershell Install-CFFeatures
+
+echo "Disconnect."
+exit
