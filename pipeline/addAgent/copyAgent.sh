0a3900fc81173e0e5983803d76afaba95c927637
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-23 20:05:27 +0200

    s3

--- pipeline/addAgent/copyAgent.sh
+++ pipeline/addAgent/copyAgent.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Current folder: "$PWD
 
-CLONEIP=$(cat cloneip/ip.txt)
+CLONEIP=$(cat ip-1.txt)
 
 echo "Copying agent to clone at "$CLONEIP
 sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@$CLONEIP:'"/Program Files/WindowsPowerShell/Modules"'
