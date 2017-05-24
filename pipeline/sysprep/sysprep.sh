710e55eb4552a9bb039decec74055e5c5e4cfacc
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-24 15:32:28 +0200

    sysprep

---
+++ pipeline/sysprep/sysprep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+echo "Unzipping lgpo."
+unzip lgpo.zip
+
+CLONEIP=$(cat redis-values/cloneip)
+echo "Connecting to cell VM at "$CLONEIP
+remote_cmd=(sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP)
+
+echo "Copying lgpo.exe to Cell windows folder."
+sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no LGPO.exe  $VMUSER@$CLONEIP:'"/Windows"'
+
+echo "Invoke sysprep."
+"${remote_cmd[@]}" 'powershell -NonInteractive -ExecutionPolicy Unrestricted -Command "&{Invoke-Sysprep -IaaS vsphere -NewPassword "'$VMPASS'" -ProductKey "'$PRODUCTKEY'" -Owner "'$VMUSER'" -Organization "'$ORG'"}"'
+
+exit
