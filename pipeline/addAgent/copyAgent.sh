4c07f22f4a9ed50150bb74ad7c9cd98e3d17e18b
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-24 13:46:13 +0200

    tired of paths

--- pipeline/addAgent/copyAgent.sh
+++ pipeline/addAgent/copyAgent.sh
@@ -4,6 +4,6 @@ set -e
 
 CLONEIP=$(cat redis-values/cloneip)
 echo "Copying agent to clone at "$CLONEIP
-sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@$CLONEIP:'"/temp"'
+sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@$CLONEIP:'"/Program Files/WindowsPowerShell/Modules"'
 
 echo "Agent zip copied to VM."
