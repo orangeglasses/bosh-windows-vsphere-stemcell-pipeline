6a38ce8225c4e19478c5127831b7f7166e8c22f4
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-24 12:49:51 +0200

    powershell command inline

--- pipeline/prepareCell/prepareCell.sh
+++ pipeline/prepareCell/prepareCell.sh
@@ -10,6 +10,6 @@ echo "Connecting to cell VM at "$CLONEIP
 echo "Installing CF Features."
 # powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures
 #$REMOTEEXEC 'powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures'
-sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP 'powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures'
+sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP 'powershell -NonInteractive -ExecutionPolicy Unrestricted -Command "&{Install-CFFeatures}"'
 
 exit
