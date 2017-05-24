8ea6694790f051e83d11135317502b4d6e58af48
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-24 12:06:09 +0200

    .

--- pipeline/prepareCell/prepareCell.sh
+++ pipeline/prepareCell/prepareCell.sh
@@ -5,10 +5,11 @@ set -e
 CLONEIP=$(cat redis-values/cloneip)
 echo "Connecting to cell VM at "$CLONEIP
 
-REMOTEEXEC=sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP
+#REMOTEEXEC=sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP
 
 echo "Installing CF Features."
 # powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures
-$REMOTEEXEC 'powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures'
+#$REMOTEEXEC 'powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures'
+sshpass -p $VMPASS ssh -o StrictHostKeyChecking=no $VMUSER@$CLONEIP 'powershell -NonInteractive -ExecutionPolicy Unrestricted -File Install-CFFeatures'
 
 exit
