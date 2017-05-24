9e64b9e5de8e28411f8370846ba386ce08d458f7
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-24 11:43:25 +0200

    test where does redis key end up

--- pipeline/buildPSModules/copyModules.sh
+++ pipeline/buildPSModules/copyModules.sh
@@ -6,6 +6,8 @@ echo "Unzipping modules"
 mkdir BOSHmodulesExtracted
 unzip -o BOSHmodules/* -d BOSHmodulesExtracted/
 
+ls -lrt
+
 CLONEIP=$(cat cloneip)
 echo "Copying modules to clone at "$CLONEIP
 sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHmodulesExtracted/* $VMUSER@$CLONEIP:'"/Program Files/WindowsPowerShell/Modules"'
