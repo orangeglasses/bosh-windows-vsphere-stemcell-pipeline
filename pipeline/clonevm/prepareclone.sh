7e13fad4b03f4db941d5cb6fbb8054929b6b00e1
Ruurd Keizer <ruurdk@hotmail.com>
2017-05-31 15:23:45 +0200

    underscores

--- pipeline/clonevm/prepareclone.sh
+++ pipeline/clonevm/prepareclone.sh
@@ -4,11 +4,11 @@ VROAPI=$VROENDPOINT"/d9ad2397-ac07-444d-978e-5f86c07f09d5/executions"
 echo "Starting clone workflow at "$VROAPI" with user "$VROUSER
 
 echo "Clone settings:"
-echo "Host: "$CLONE_HOST
-echo "Cluster: "$CLONE_CLUSTER
+echo "Host: "$CLONEHOST
+echo "Cluster: "$CLONECLUSTER
 echo "Base Image: "$BASEVM
 #TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{}] }' $VROAPI | grep Location: | cut -d' ' -f2 )
-TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{"description":"string","name":"clusterName","scope":"local","type":"string","value":{"string":{"value":'$CLONE_CLUSTER'}}},{"description":"string","name":"hostName","scope":"local","type":"string","value":{"string":{"value":'$CLONE_HOST'}}},{"description":"string","name":"vmName","scope":"local","type":"string","value":{"string":{"value":'$BASEVM'}}}] }' $VROAPI | grep Location: | cut -d' ' -f2 )
+TOKEN=$(curl -s -D - -u $VROUSER:$VROPASS -k -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  "parameters": [{"description":"string","name":"clusterName","scope":"local","type":"string","value":{"string":{"value":'$CLONECLUSTER'}}},{"description":"string","name":"hostName","scope":"local","type":"string","value":{"string":{"value":'$CLONEHOST'}}},{"description":"string","name":"vmName","scope":"local","type":"string","value":{"string":{"value":'$BASEVM'}}}] }' $VROAPI | grep Location: | cut -d' ' -f2 )
 echo "Status token: "$TOKEN
 
 # eliminate CR from result.
