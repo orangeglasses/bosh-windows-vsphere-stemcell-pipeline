d14f5da6347a790b85bdbc91c1918864c6209003
Ruurd Keizer <ruurdk@hotmail.com>
2017-07-03 09:37:59 +0200

    Skip manifest check and verification.

--- pipeline/preparestemcell/extractOVA.sh
+++ pipeline/preparestemcell/extractOVA.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Extracting VM as OVA."
-ovftool --noSSLVerify --shaAlgorithm=sha1 vi://$VCUSER:$VCPASS@$VCHOST/dc/vm/WindowsStemClone ovadir/WindowsStemClone.ova
+ovftool --noSSLVerify --disableVerification --skipManifestGeneration vi://$VCUSER:$VCPASS@$VCHOST/dc/vm/WindowsStemClone ovadir/WindowsStemClone.ova
