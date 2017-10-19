0b01cff62a04e5164907aca2b9821473cafade1b
Ruurd Keizer <Ruurd.Keizer@rws.nl>
2017-10-19 12:03:13 +0200

    ipv6

--- pipeline/addAgent/copyAgent.sh
+++ pipeline/addAgent/copyAgent.sh
@@ -4,6 +4,10 @@ set -e
 
 CLONEIP=$(cat redis-values/cloneip)
 echo "Copying agent to clone at "$CLONEIP
-sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@$CLONEIP:'"/Program Files/WindowsPowerShell/Modules"'
+if [[ $CLONEIP == *":"* ]]; then
+  sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@\[$CLONEIP\]:'"/Program Files/WindowsPowerShell/Modules"'
+else
+  sshpass -p $VMPASS scp -r -o StrictHostKeyChecking=no BOSHagent/* $VMUSER@$CLONEIP:'"/Program Files/WindowsPowerShell/Modules"'
+fi
 
 echo "Agent zip copied to VM."
