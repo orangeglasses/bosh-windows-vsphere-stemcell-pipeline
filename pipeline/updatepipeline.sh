f55a17e47410062e0ef8d04ce03319db2511a2a6
Ruurd Keizer <Ruurd.Keizer@rws.nl>
2017-12-06 11:01:10 +0100

    pipeline triggering

--- pipeline/updatepipeline.sh
+++ pipeline/updatepipeline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fly -t concourse set-pipeline -p windowsstem -c windowsstem.yml --load-vars-from ../../secrets.yml
+fly -t concourse set-pipeline -p windowsstem-2012 -c windowsstem.yml --load-vars-from ../../secrets.yml
